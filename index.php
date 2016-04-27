8ae87e4e8af5fcb388414f550d6a36cb1188fe06
Vlad Feninets <v.feninets@lazardexport.com>
2016-04-27 17:50:59 +0300

    working shop with descr on buy

--- index.php
+++ index.php
@@ -21,49 +21,14 @@
 
 
 				<div class="col-lg-12">
-				            <div class="page-header">
-				              <h1 id="tables">Products</h1>
-				            </div>
+		            <div class="page-header">
+		              <h1 id="tables">Products <span data-remodal-target="add_drug" class="plus_sign">+</span></h1>
+		            </div>
 
-				            <div class="bs-component">
-				              <table class="table table-striped table-hover ">
-				                <thead>
-				                  <tr>
-				                    <th>#</th>
-				                    <th>User</th>
-				                    <th>Datetime</th>
-				                    <th>Price</th>
-				                  </tr>
+		            <div class="bs-component" id="items_table">
 
-				                  <tr>
-				                  	<td id="test">1</td>
-				                  	<td>Andrew</td>
-				                  	<td>10.01.2016 16:00</td>
-				                  	<td>140 $</td>
-				                  </tr>
-
-				                  <tr>
-				                  	<td>1</td>
-				                  	<td>George</td>
-				                  	<td>12.01.2016 11:10</td>
-				                  	<td>100 $</td>
-				                  </tr>
-
-				                  <tr>
-				                  	<td>1</td>
-				                  	<td>Scott</td>
-				                  	<td>22.02.2016 12:03</td>
-				                  	<td>101 $</td>
-				                  </tr>
-
-
-				                </thead>
-				                <tbody id="insert_products">
-				                  
-				                </tbody>
-				              </table> 
-				            <div id="source-button" class="btn btn-primary btn-xs" style="display: none;">&lt; &gt;</div></div><!-- /example -->
-				          </div>
+		            <div id="source-button" class="btn btn-primary btn-xs" style="display: none;">&lt; &gt;</div></div><!-- /example -->
+				</div>
 			</div>
 		</div>
 	</div>	
